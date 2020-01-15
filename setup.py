2f86a10f7254a0eab1c3d05081eb074a9b26ce34
Julien Romero <romerojulien34@gmail.com>
2020-01-15 14:07:31 +0100

    add all requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import find_packages, setup
 
+requirements = []
+with open("requirements.txt") as f:
+    for line in f:
+        requirements.append(line.strip())
+
 setup(
     name='quasimodo website',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
-        'flask-bootstrap',
-        'flask-sqlalchemy',
-        'flask-migrate',
-        'flask-wtf',
-        'flask_fontawesome'
-    ],
+    install_requires=requirements,
 )
